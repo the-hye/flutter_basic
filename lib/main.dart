b83be1be83cb32916ff67bcf3be75a8494e48ee8
the-hye <ganzi410@gmail.com>
2023-09-02 16:25:50 +0900

    study for MaterialApp & Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(icon: Icon(Icons.home), onPressed: () {
+            print('Tab!');
+          },),
+          Icon(Icons.play_arrow),
+        ],
+        centerTitle: false,
+        title: Text('This is App bar'),
+      ),
       body: TestWidget(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.bug_report),
+        onPressed: () {
+          print('Tab! FAB!');
+        },
+      ),
     ),
   ));
 }
@@ -16,9 +32,9 @@ class TestWidget extends StatelessWidget {
     return SafeArea(
       child: Center(
         child: Text(
-          'Hello Flutter',
+          'Hello Flutter!',
           style: TextStyle(
-            fontSize: 40,
+            fontSize: 50,
             color: Colors.black,),
         ),
       ),
